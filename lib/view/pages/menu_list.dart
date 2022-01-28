fe02342485c98e382cc822f93d4fb3751c50e6ad
IQBALARRAFI6706204091 <iqbalarrafi@student.telkomuniversity.ac.id>
2022-01-29 00:12:40 +0700

    fixed

--- lib/view/pages/menu_list.dart
+++ lib/view/pages/menu_list.dart
@@ -31,64 +31,95 @@ class _MenuListState extends State<MenuList> {
       body: Container(
         child: Stack(
           children: [
-            GridView.builder(
-              itemCount: ImageList.imageList.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-              ),
-              itemBuilder: (context, i) => InkWell(
-                onTap: () {
-                  _galleryController.modalOpen(ImageList.imageList[i].image,
-                      ImageList.imageList[i].imageName);
-                },
-                child: Image.asset(
-                  "assets/images/" + ImageList.imageList[i].image,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Obx(
-              () => _galleryController.viewImage.value != ""
-                  ? Container(
-                      child: Stack(
-                        children: [
-                          Opacity(
-                            opacity: 0.6,
-                            child: GestureDetector(
-                              onTap: () {
-                                _galleryController.modalClose();
-                              },
-                              child: Container(
-                                color: Colors.black,
-                              ),
-                            ),
-                          ),
-                          Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image.asset(
-                                    "assets/images/" +
-                                        _galleryController.viewImage.string,
-                                    height: MediaQuery.of(context).size.height *
-                                        0.7),
-                                Text(
-                                  _galleryController.imageName.string,
-                                  style: textWhiteImage,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  : Opacity(opacity: 0),
-            ),
+            GridImage(galleryController: _galleryController),
+            ModalImage(galleryController: _galleryController),
           ],
         ),
       ),
+    );
+  }
+}
+
+class GridImage extends StatelessWidget {
+  const GridImage({
+    Key? key,
+    required GalleryController galleryController,
+  }) : _galleryController = galleryController, super(key: key);
+
+  final GalleryController _galleryController;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: ImageList.imageList.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+      ),
+      itemBuilder: (context, i) => InkWell(
+        onTap: () {
+          _galleryController.modalOpen(ImageList.imageList[i].image,
+              ImageList.imageList[i].imageName);
+        },
+        child: Image.asset(
+          "assets/images/" + ImageList.imageList[i].image,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class ModalImage extends StatelessWidget {
+  const ModalImage({
+    Key? key,
+    required GalleryController galleryController,
+  }) : _galleryController = galleryController, super(key: key);
+
+  final GalleryController _galleryController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => _galleryController.viewImage.value != ""
+          ? Container(
+              child: Stack(
+                children: [
+                  Opacity(
+                    opacity: 0.6,
+                    child: GestureDetector(
+                      onTap: () {
+                        _galleryController.modalClose();
+                      },
+                      child: Container(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                            "assets/images/" +
+                                _galleryController.viewImage.string,
+                            height: MediaQuery.of(context).size.height *
+                                0.7),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          _galleryController.imageName.string,
+                          style: textWhiteImage,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Opacity(opacity: 0),
     );
   }
 }
